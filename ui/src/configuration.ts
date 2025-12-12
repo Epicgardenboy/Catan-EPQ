51585eb97f41b1418f6fa0ec3a1d4531167ca7a9
EpicGardenboy <61985920+Epicgardenboy@users.noreply.github.com>
2025-12-12 20:40:38 +0000

    Localhost fix

--- ui/src/configuration.ts
+++ ui/src/configuration.ts
@@ -1,2 +1,8 @@
+// If running with localhost use localhost stuff
+
+const isLocalDev = typeof window !== 'undefined' && 
+  (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') &&
+  window.location.port === '3000';
+
 export const API_URL: string =
-  import.meta.env.CTRON_API_URL || "";
+  import.meta.env.CTRON_API_URL || (isLocalDev ? "http://localhost:5001" : "");
